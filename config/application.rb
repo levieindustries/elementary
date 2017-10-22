cec8afcaba770e6a742fae5bd11b891c845d72e2
Lars Levie <larslevie@gmail.com>
2017-10-22 13:17:20 -0500

    Upgrade Rails to 5.1.4

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Elementary
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     config.generators.test_framework false
   end
 end
